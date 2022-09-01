0598e61f738b4c20ac619d7710a64b8aa6e60867
J. Pitherin <jessica.pitherin@gmail.com>
2022-09-01 18:14:38 +0300

    Give pick/sword, give apples, give named cat

--- Minecraft/mc_bot/Sophies_mc_bot.py
+++ Minecraft/mc_bot/Sophies_mc_bot.py
@@ -3,7 +3,7 @@ import sys
 
 HOST = 'localhost'  # Hostname of the Minecraft server
 PORT = 25565  # Port number of the RCON server
-me = "Sophie__Games"
+ME = "Sophie__Games"
 
 # Create the RCONClient:
 
@@ -13,8 +13,7 @@ def login():
 
     # Login to RCON:
     if rcon.login("HewwoWorld"):
-        print("RCON Connected")
-        
+        #print("RCON Connected")
         return True
     else:
         print("RCON Failed to Connect")
@@ -22,22 +21,101 @@ def login():
     
 
 def MakeItRain(chatter):
-    print("chatter", chatter)
+    #print("chatter", chatter)
 
     rcon.start()
     response = rcon.command("weather rain")
     response = rcon.command(f"say {chatter} made it rain")
     rcon.stop()
     
+def BlueSkies4U(chatter):
+    rcon.start()
+    response = rcon.command("weather clear")
+    response = rcon.command(f"say {chatter} chased away the rain")
+    rcon.stop()
     
+def StormsABrewin(chatter):
+    rcon.start()
+    response = rcon.command("weather thunder")
+    response = rcon.command(f"say Prepare for lighting, courtesy of {chatter}")
+    rcon.stop()
+
+def GiveWoodPick(chatter):
+    rcon.start()
+    response = rcon.command(f"give {ME} minecraft:wooden_pickaxe")
+    response = rcon.command(f"say {chatter} gives Sophie a wooden pickaxe")
+    rcon.stop()  
+    
+def GiveIronPick(chatter):
+    rcon.start()
+    response = rcon.command(f"give {ME} minecraft:iron_pickaxe")
+    response = rcon.command(f"say {chatter} gives Sophie an iron pickaxe")
+    rcon.stop() 
+
+def GiveWoodSword(chatter):
+    rcon.start()
+    response = rcon.command(f"give {ME} minecraft:wooden_sword")
+    response = rcon.command(f"say {chatter} gives Sophie a wooden sword")
+    rcon.stop()   
+
+def GiveIronSword(chatter):
+    rcon.start()
+    response = rcon.command(f"give {ME} minecraft:iron_sword")
+    response = rcon.command(f"say {chatter} gives Sophie an iron sword")
+    rcon.stop()     
+
+def GiveApples(chatter):
+    rcon.start()
+    response = rcon.command(f"give {ME} minecraft:apple 3")
+    response = rcon.command(f"say {chatter} gives Sophie some apples")
+    rcon.stop()
+    
+def AccioKitty(chatter,name):
+    rcon.start()
+    cmd = "summon cat ~ ~ ~ "
+    # /summon cat ~ ~ ~ {CustomName:"\"Daisy\"",Owner:Sophie__Games}
+    
+    if name != ".":
+        cmd = cmd + '{CustomName:"\\"' + name + '\\"",Owner:Sophie__Games}'
+    else:
+        cmd = cmd + "{Owner:Sophie__Games}"
+        
+    print(cmd)
+    
+    response = rcon.command(cmd)
+    response = rcon.command(f"say {chatter} thinks Sophie needs a kitty")
+    if name != ".":
+        response = rcon.command(f"say Meet {name}")
+    rcon.stop()
     
 if __name__ == "__main__":
     chatter = sys.argv[1]
     command = sys.argv[2]
+    name = sys.argv[3]
     
     success = login()
     
     if success == True:
         print("command", command)
+        
         if command == "MakeItRain":
             MakeItRain(chatter)
+        if command == "BlueSkies4U":
+            BlueSkies4U(chatter)
+        if command == "StormsABrewin":
+            StormsABrewin(chatter)
+        
+        if command == "GiveWoodPick":
+            GiveWoodPick(chatter)
+        if command == "GiveIronPick":
+            GiveIronPick(chatter)
+        if command == "GiveWoodSword":
+            GiveWoodSword(chatter)
+        if command == "GiveIronSword":
+            GiveIronSword(chatter)
+            
+        if command == "GiveApples":
+            GiveApples(chatter)
+        
+        if command == "AccioKitty":
+            AccioKitty(chatter,name)
